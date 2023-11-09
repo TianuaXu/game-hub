9c0d3439b1ce87a14ddcea3aaa596339ffe5ee30
CanioV <2756432588@qq.com>
2023-11-09 14:26:53 +0800

    refactoring fetch-game with react-query

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,13 @@
-import axios from 'axios';
+import axios from "axios"
+
+export interface FetchResponse<T> {
+    count: number
+    results: T[]
+}
 
 export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
+    baseURL: "https://api.rawg.io/api",
     params: {
-        key: '06c23af82b304396bff19ee48849fd6d',
+        key: "06c23af82b304396bff19ee48849fd6d",
     },
-});
+})
