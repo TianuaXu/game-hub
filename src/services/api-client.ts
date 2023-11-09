909c59602088bc68b028384cdaaab6d8d7edad7d
CanioV <2756432588@qq.com>
2023-11-09 14:43:42 +0800

    refactor duplicated apiclient

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,13 +1,27 @@
-import axios from "axios"
+import axios, { Axios, AxiosRequestConfig } from "axios"
 
 export interface FetchResponse<T> {
     count: number
     results: T[]
 }
 
-export default axios.create({
+const axiosInstance = axios.create({
     baseURL: "https://api.rawg.io/api",
     params: {
         key: "06c23af82b304396bff19ee48849fd6d",
     },
 })
+
+class ApiClient<T> {
+    endpoint: string
+
+    constructor(endpoint: string) {
+        this.endpoint = endpoint
+    }
+
+    getAll = (config?: AxiosRequestConfig) => {
+        return axiosInstance.get<FetchResponse<T>>(this.endpoint, config).then((res) => res.data)
+    }
+}
+
+export default ApiClient
