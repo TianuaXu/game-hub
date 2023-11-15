1617b1e7c500e6afe766e618f183e5331a3d70d6
CanioV <2756432588@qq.com>
2023-11-15 18:56:36 +0800

    Handle Error page

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom"
 import Layout from "./pages/Layout"
 import HomePage from "./pages/HomePage"
 import GameDetailPage from "./pages/GameDetailPage"
+import ErroPage from "./pages/ErroPage"
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErroPage />,
     children: [
       {
         // index: true means this is the default route
