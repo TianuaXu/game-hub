10587df8f828a8403bd17e8f1dcc2ea4c9f1acfc
CanioV <2756432588@qq.com>
2023-11-18 23:33:35 +0800

    Fix NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,17 +2,20 @@ import { HStack, Image } from "@chakra-ui/react"
 import logo from "../assets/logo.webp"
 import ColorModeSwitch from "./ColorModeSwitch"
 import SearchInput from "./SearchInput"
+import { Link } from "react-router-dom"
 
 const NavBar = () => {
-    return (
-        // HStack is a horizontal stack
-        <HStack padding='10px'>
-            {/* boxSize is a shorthand for width and height */}
-            <Image src={logo} boxSize='60px' />
-            <SearchInput />
-            <ColorModeSwitch />
-        </HStack>
-    )
+  return (
+    // HStack is a horizontal stack
+    <HStack padding='10px'>
+      {/* boxSize is a shorthand for width and height */}
+      <Link to='/'>
+        <Image src={logo} boxSize='60px' objectFit='cover' />
+      </Link>
+      <SearchInput />
+      <ColorModeSwitch />
+    </HStack>
+  )
 }
 
 export default NavBar
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,33 +2,32 @@ import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react"
 import { useRef } from "react"
 import { BsSearch } from "react-icons/bs"
 import useGameQueryStore from "../store"
+import { useNavigate } from "react-router-dom"
 
 const SearchInput = () => {
-    // useRef is a hook that returns a mutable ref object whose .current property is initialized to the passed argument (initialValue). The returned object will persist for the full lifetime of the component.
-    // ref doesn't trigger re-render
-    const ref = useRef<HTMLInputElement>(null)
-    // the only dependency is setSearchText
-    const setSearchText = useGameQueryStore((s) => s.setSearchText)
-    return (
-        <form
-            onSubmit={(event) => {
-                event.preventDefault()
-                if (ref.current) {
-                    setSearchText(ref.current.value)
-                }
-            }}
-        >
-            <InputGroup>
-                <InputLeftElement children={<BsSearch />} />
-                <Input
-                    ref={ref}
-                    borderRadius={20}
-                    placeholder='Search game...'
-                    variant='filled'
-                ></Input>
-            </InputGroup>
-        </form>
-    )
+  // useRef is a hook that returns a mutable ref object whose .current property is initialized to the passed argument (initialValue). The returned object will persist for the full lifetime of the component.
+  // ref doesn't trigger re-render
+  const ref = useRef<HTMLInputElement>(null)
+  // the only dependency is setSearchText
+  const setSearchText = useGameQueryStore((s) => s.setSearchText)
+
+  const navigate = useNavigate()
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault()
+        if (ref.current) {
+          setSearchText(ref.current.value)
+          navigate("/")
+        }
+      }}
+    >
+      <InputGroup>
+        <InputLeftElement children={<BsSearch />} />
+        <Input ref={ref} borderRadius={20} placeholder='Search game...' variant='filled'></Input>
+      </InputGroup>
+    </form>
+  )
 }
 
 export default SearchInput
